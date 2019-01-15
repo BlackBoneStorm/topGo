230bae5e71e4552b797893ce3a47b1dc4a09d2d5
BlackBoneStorm <41055339+BlackBoneStorm@users.noreply.github.com>
2019-01-15 10:20:26 +0200

    topGo

--- 3_if_else.go
+++ 3_if_else.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Sanya, Dorou")
+
+	if 5%2 == 0 {
+		fmt.Println("it is pair")
+	} else {
+		fmt.Println("it is not pair")
+	}
+
+	if 7%4 == 0 {
+		fmt.Println("8 % 4")
+	} else {
+		fmt.Println("!% 4")
+	}
+
+
 }
